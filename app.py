29fde5596aa7c168f1d63a063ce94db58f12d688
Harsh-maker007 <harshshandilya01@gmail.com>
2026-07-21 18:53:49 +0530

    Fix ZeroDivisionError when overs is 0

--- app.py
+++ app.py
@@ -45,8 +45,8 @@ if st.button('Predict Probability'):
     runs_left = target - score
     balls_left = 120 - (overs*6)
     wickets = 10 - wickets
-    crr = score/overs
-    rrr = (runs_left*6)/balls_left
+    crr = score / overs if overs > 0 else 0
+    rrr = (runs_left * 6) / balls_left if balls_left > 0 else 0
 
     input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'city':[selected_city],'runs_left':[runs_left],'balls_left':[balls_left],'wickets':[wickets],'total_runs_x':[target],'crr':[crr],'rrr':[rrr]})
 
